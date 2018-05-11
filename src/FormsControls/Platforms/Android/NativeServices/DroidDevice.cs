27487106235056c170a4f5410ab47223010ff678
onikiri2007 <onikiri2007@gmail.com>
2018-05-11 11:10:02 +1000

    netstandard

---
+++ src/FormsControls/Platforms/Android/NativeServices/DroidDevice.cs
@@ -0,0 +1,31 @@
+﻿using Android.Content;
+using Android.Util;
+using FormsControls.Base;
+using FormsControls.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(DroidDevice))]
+namespace FormsControls.Droid
+{
+    public class DroidDevice : IDeviceInfo
+    {
+        private static Context Context => Android.App.Application.Context;
+
+        public static DisplayMetrics Metrics => Context.Resources.DisplayMetrics;
+
+        private static float ActionBarHeight
+        {
+            get
+            {
+                var styledAttributes = Context.Theme.ObtainStyledAttributes( new[] { Android.Resource.Attribute.ActionBarSize });
+                var actionbarHeight = styledAttributes.GetDimension(0, 0);
+                styledAttributes.Recycle();
+                return actionbarHeight;
+            }
+        }
+
+        public double ScreenHeight => Metrics.HeightPixels - ActionBarHeight;
+
+        public double ScreenWidth => Metrics.WidthPixels;
+    }
+}
